52dd12f11817f367e41194dade243a80f56b26f9
Mohammed Farmaan <mohammed.farmaan.k@gmail.com>
2022-10-11 13:07:46 +0530

    Initial commit

---
+++ src/PongFrame.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class PongFrame extends JFrame{
+
+    PongPanel panel;
+    PongFrame(){
+        panel = new PongPanel();
+        this.add(panel);
+        this.setTitle("Pong Game");
+        this.setResizable(false);
+        this.setBackground(Color.BLACK);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+}
